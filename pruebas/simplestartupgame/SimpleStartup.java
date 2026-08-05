f329f3882045ff827b66ac934a244cde4bf4a8c3
Ander0296 <165844215+Ander0296@users.noreply.github.com>
2026-08-05 16:03:23 -0500

    Continuar en la pagina 361, java-s41

--- pruebas/simplestartupgame/SimpleStartup.java
+++ pruebas/simplestartupgame/SimpleStartup.java
@@ -1,26 +1,50 @@
 public class SimpleStartup {
 
     int[] locationCells;
-    boolean[] repeated;
     int numOfHits;
+    boolean[] posiciones = new boolean[3];
+    int posicion;
 
     public String checkYourself(int guess) {
         String result = "miss";
 
-        for (int cell : locationCells) {
-            if (guess == cell) {
+        // for (int cell : locationCells) {
+        // if (guess == cell) {
+        //
+        // result = "hit";
+        // numOfHits++;
+        // break;
+        // } // end if
+        // } // end for
+        //
+
+        for (int i = 0; i < locationCells.length; i++) {
+
+            if (posiciones[i] == true) {
+                System.out.println("Numero repetido");
+            } else if (guess == locationCells[i] && posiciones[i] == false) {
 
                 result = "hit";
-                for (int i = 0; i < locationCells.length; i++) {
-                    if (guess == i) {
-                        repeated[i] = true;
-                        numOfHits++;
-                        break;
-                    }
-                }
-            } // end if
-        } // end for
+                numOfHits++;
+                posiciones[i] = true;
+                break;
+            }
 
+        }
+
+        // for (int i = 0; i < locationCells.length; i++) {
+        // if (guess == locationCells[i]) {
+        // if (posiciones[i] == true) {
+        // System.out.println("Numero repetido");
+        // } else {
+        // result = "hit";
+        // numOfHits++;
+        // posiciones[i] = true;
+        // }
+        // break;
+        // }
+        // }
+        //
         if (numOfHits == locationCells.length) {
             result = "kill";
         } // end if
